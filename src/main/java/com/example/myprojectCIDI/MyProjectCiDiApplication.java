4b370bac6818924f8028eb7a70b214a905c44e65
iamyd <yemane.berhane@miu.edu>
2023-09-20 11:09:06 -0500

    cloud native springboot app created

---
+++ src/main/java/com/example/myprojectCIDI/MyProjectCiDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.myprojectCIDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectCiDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectCiDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myprojectCIDI/MyProjectCiDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprojectCIDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectCiDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
